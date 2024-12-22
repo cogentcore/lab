b3275b354b353de0249bc170164b357553c286b4
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-12-22 14:53:36 -0800

    start on docs

---
+++ docs/docs.go
@@ -0,0 +1,24 @@
+// Copyright (c) 2024, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"embed"
+
+	"cogentcore.org/core/core"
+	"cogentcore.org/core/pages"
+)
+
+//go:embed content
+var content embed.FS
+
+func main() {
+	b := core.NewBody("Cogent Lab")
+	pg := pages.NewPage(b).SetContent(content)
+	b.AddTopBar(func(bar *core.Frame) {
+		core.NewToolbar(bar).Maker(pg.MakeToolbar)
+	})
+	b.RunMainWindow()
+}
